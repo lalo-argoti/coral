91fa9e678b2503f958eccb1aef1b08e92001ae6a
Juan Felipe Orozco Cortés <juan.orozcoco@gmail.com>
2024-11-28 16:05:16 -0500

    Agregando views.py al repositorio

--- app/alumnos/routes.py
+++ app/alumnos/routes.py
@@ -1,7 +1,9 @@
 from flask import render_template
 from . import alumnos
+from .views import obtener_lista_alumnos  # Importa la función auxiliar
 
 @alumnos.route('/alumnos')
 def lista_alumnos():
-    # Aquí podrías obtener datos de la base de datos
-    return render_template('alumnos/lista_alumnos.html')
+    # Llama a la función que obtiene los datos
+    datos_alumnos = obtener_lista_alumnos()
+    return render_template('alumnos/lista_alumnos.html', alumnos=datos_alumnos)
