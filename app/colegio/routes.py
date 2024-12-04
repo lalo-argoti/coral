8f95df9472fbe1efaa7b9b1386843107dfb7d834
Juan Felipe Orozco Cortés <juan.orozcoco@gmail.com>
2024-12-04 10:47:58 -0500

    Se agregaron los datos de la base de datos

--- app/colegio/routes.py
+++ app/colegio/routes.py
@@ -36,9 +36,62 @@ def eventos():
 def dcnt_agregar():
     return render_template('colegio/agregar.html',encabezados=[], empleados=[], username=session.get('username'))
 
-@colegio.route(p+'/docentes/ver')
+from flask import render_template, session, request
+from . import colegio
+from app.core.Mirlt import DB  # Asegúrate de que esta ruta es correcta
+import logging
+
+@colegio.route(p + '/docentes/ver')
 def dcnt_ver():
-    return render_template('core/perfil.html', username=session.get('username'))
+    # Obtener el parámetro 'sujeto' de la URL
+    sujeto = request.args.get('sujeto', default=None, type=int)
+
+    # Construir la consulta SQL basada en la presencia del parámetro 'sujeto'
+    if sujeto:
+        query = f'''SELECT CC_NUMERO, NOMBRES, APELLIDOS, FECHA_NAC, CARGO, LUG_EXP, EST_CIVIL, CC_LUGAR, LIB_MILIT, GENERO, DIRECCION, TELEFONO, FEC_VINCUL, DECRETO, UNIVERS, CURSILLOS, NOM_GRADO1, REG_GRADO1, ESCAL_SE, email FROM  occb_profesor WHERE CC_NUMERO = %s '''
+        params = (sujeto,)
+    else:
+        query = '''SELECT CC_NUMERO, NOMBRES, APELLIDOS, FECHA_NAC, CARGO, LUG_EXP, EST_CIVIL, CC_LUGAR, LIB_MILIT, GENERO, DIRECCION, TELEFONO, FEC_VINCUL, DECRETO, UNIVERS, CURSILLOS, NOM_GRADO1, REG_GRADO1, ESCAL_SE, email FROM  occb_profesor ORDER BY  APELLIDOS, NOMBRES;'''
+        params = None
+
+    # Ejecutar la consulta utilizando tu clase DB
+    try:
+        db = DB(query=query, username="")  # Asegúrate de pasar el username si es necesario
+        empleados = db.run_query(query=query, params=params) if params else db.run_query()
+    except Exception as e:
+        logging.error(f'Error al ejecutar la consulta: {e}')
+        empleados = []
+
+    # Mapea los resultados a una lista de diccionarios para facilitar el acceso en la plantilla
+    profesores = []
+    for row in empleados:
+        profesor = {
+            "CC_NUMERO": row[0],
+            "NOMBRES": row[1],
+            "APELLIDOS": row[2],
+            "FECHA_NAC": row[3],
+            "CARGO": row[4],
+            "LUG_EXP": row[5],
+            "EST_CIVIL": row[6],
+            "CC_LUGAR": row[7],
+            "LIB_MILIT": row[8],
+            "GENERO": row[9],
+            "DIRECCION": row[10],
+            "TELEFONO": row[11],
+            "FEC_VINCUL": row[12],
+            "DECRETO": row[13],
+            "UNIVERS": row[14],
+            "CURSILLOS": row[15],
+            "NOM_GRADO1": row[16],
+            "REG_GRADO1": row[17],
+            "ESCAL_SE": row[18],
+            "email": row[19],
+        }
+        profesores.append(profesor)
+
+    # Renderizar la plantilla con los datos de los profesores
+    return render_template('core/perfil.html', profesores=profesores, username=session.get('username'))
+
     
 @colegio.route(p+'/docentes/guardar')
 def dcnt_guardar():
