6bb7406ba54241609ada57c4c887ece8e2c04521
lalo-argoti <carlos.e.argoti@gmail.com>
2024-12-04 17:48:08 +0000

    Nov 4

--- app/coral/routes.py
+++ app/coral/routes.py
@@ -23,15 +23,19 @@ def databases():
 def tablas():
      encabezados=["Tabla", ""]
      datos= DB("SHOW TABLES;", username="").run_query()
-     return render_template('core/tabla.html',datos=datos, encabezados=encabezados,titulo="tablas",link2= "url_for('coral.datos',datos=eval(datos[0]))",  username=session.get('username'))
+         # Agregar el enlace dinámico para cada tabla, si se agrega esto , link2 debe quedar como True
+     datos= [
+        [tabla[0] , url_for('coral.datos', ref=tabla[0])]        for tabla in datos
+     ]
+     return render_template('core/tabla.html',datos=datos, encabezados=encabezados,titulo="tablas", link2=True,  username=session.get('username'))
 
 
-@coral.route('/tablas/datos/<string:datos>')
-def datos(datos):
-     c_encabezados=  DB(f"DESCRIBE {datos};", username="").run_query()
+@coral.route('/tablas/datos/<string:ref>')
+def datos(ref):
+     c_encabezados=  DB(f"DESCRIBE {ref};", username="").run_query()
      encabezados=  [columna[0] for columna in c_encabezados]
-     datos= DB(f"SELECT * FROM {datos};", username="").run_query()
-     return render_template('core/tabla.html',datos=datos, encabezados=encabezados,titulo=f"{datos}",link=url_for('coral.datos') , username=session.get('username'))
+     datos= DB(f"SELECT * FROM {ref};", username="").run_query()
+     return render_template('core/tabla.html',datos=datos, encabezados=encabezados,titulo=ref,link=url_for('coral.tablas') , username=session.get('username'))
 
 # Ruta para procesar consultas SQL
 # Ruta para procesar consultas SQL
