e0bd3c207b0729b2ad3cb11f24bfb84d80eb0dc7
Carlos Argoti <150752226+lalo-argoti@users.noreply.github.com>
2024-12-05 11:17:31 -0500

    Update routes.py

--- app/materias/routes.py
+++ app/materias/routes.py
@@ -2,33 +2,20 @@ from flask import render_template,session
 from . import materias
 from .views import *
 
-@materias.route('/materias')
-def r_portal():
-    # Aquí podrías obtener datos de la base de datos
-    menu = [
-    {'texto1': 'Distribución', 'texto2': 'de horarios', 'imagen': 'distribucion.png', 'link': 'grupos.distribucion'},
-    {'texto1': 'Logros', 'texto2': 'académicos', 'imagen': 'logros.png', 'link': 'materias.logros'},
-    {'texto1': 'Evaluaciones', 'texto2': 'y calificaciones', 'imagen': 'evaluaciones.png', 'link': 'materias.evaluaciones'}
-    ]
-    # Renderiza una plantilla para mostrar los resultados
-    return render_template('core/portal.html', menu=menu,  username=session.get('username'))
-
 @materias.route('/materias/logros')
 def logros():
      datos= DB(f"SELECT id,CODIGO, SIGLA, FRASE  FROM occb_CCI_fras;", username="").run_query()   #WHERE colegio= mi_colegio
      return render_template('core/tabla.html',datos=datos, encabezados=["","",""],titulo="frases",link="#" , username=session.get('username'))
 
-
-
 @materias.route('/materias/evaluaciones')
 def evaluaciones():
    return ""
-
-'''
-colegio.route('/matriculas')
-def almns_matriculas():
-    # Aquí podrías obtener datos de la base de datos
-    resultados = matriculas()
-    # Renderiza una plantilla para mostrar los resultados
-    return render_template('colegio/matriculas.html', resultados=resultados)
-'''
+    
+@materias.route('/materias')
+def r_portal():
+    menu = [
+    {'texto1': 'Distribución', 'texto2': 'de horarios', 'imagen': 'distribucion.png', 'link': 'grupos.distribucion'},
+    {'texto1': 'Logros', 'texto2': 'académicos', 'imagen': 'logros.png', 'link': 'materias.logros'},
+    {'texto1': 'Evaluaciones', 'texto2': 'y calificaciones', 'imagen': 'evaluaciones.png', 'link': 'materias.evaluaciones'}
+    ]
+    return render_template('core/portal.html', menu=menu,  username=session.get('username'))
